367c7395ccf10a1a8eeaa7a43a3da0128451a057
Nahid Estes <44173168+NahidEstes@users.noreply.github.com>
2023-05-02 12:01:16 +0600

    teams section design completed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Banner from "./Banner/Banner";
 import ContentCard from "./ContentCard/ContentCard";
 import Footer from "../../Layouts/Footer";
+import Teams from "./Teams/Teams";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <ContentCard />
+      <Teams />
       <Footer />
     </div>
   );
